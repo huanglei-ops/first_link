23e98941a1d300489e8b853ba95e5d41db31fb51
huanglei <1563418746@qq.com>
2019-09-30 12:12:41 +0800

    all commit

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("1.c\n");
+    return 0;
+}
---
+++ 2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+
+{
+	printf("2.c\n");
+return 0;
+}
